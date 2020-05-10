773cff8c78f55b552fd08e08a05201bf07c0fc50
henrykash <henrykariuki003@gmail.com>
2020-05-10 14:35:01 -0700

    Initial commit

---
+++ app/src/main/java/com/example/contactrecyclerview/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.contactrecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //initializing the recyclerview
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
