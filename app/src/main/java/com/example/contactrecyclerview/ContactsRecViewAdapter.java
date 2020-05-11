d41eb2e88e4693360607c302465e67b5e56c054a
henrykash <henrykariuki003@gmail.com>
2020-05-11 01:46:12 -0700

    third commit

--- app/src/main/java/com/example/contactrecyclerview/ContactsRecViewAdapter.java
+++ app/src/main/java/com/example/contactrecyclerview/ContactsRecViewAdapter.java
@@ -1,30 +1,59 @@
 package com.example.contactrecyclerview;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
 public class ContactsRecViewAdapter extends RecyclerView.Adapter<ContactsRecViewAdapter.ViewHolder>{
-    //initialize the array list and pass to it Contact class in order to avoid null pointer exception
+    //initialize the array list of contacts and pass to it Contact class in order to avoid null pointer exception
     private ArrayList<Contact> contacts = new ArrayList<>();
 
+    private Context context;
+
     //initialize a constructor for this class press alt + insert
-    public ContactsRecViewAdapter() {
+    public ContactsRecViewAdapter(Context context) {
+        this.context = context;
     }
 
     @NonNull
     @Override
+    //to create an instance of a View Holder class  for every   item on a recycler view
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        //create a view object to instantiate/inflate the layout items for each new items
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.contacts_list_item, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+
+        holder.txtName.setText(contacts.get(position).getName());
+        holder.txtEmail.setText(contacts.get(position).getEmail());
+        holder.parent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context,contacts.get(position).getName() + "selected", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Glide.with(context)
+                .asBitmap()
+                .load(contacts.get(position).getImageUrl())
+                .into(holder.image);
 
     }
 
@@ -42,7 +71,9 @@ public class ContactsRecViewAdapter extends RecyclerView.Adapter<ContactsRecView
     // this inner view holder class is responsible for holding every item on the recycler view
     public class ViewHolder extends RecyclerView.ViewHolder{
 
-          private  TextView txtName;
+          private  TextView txtName , txtEmail;
+          private  CardView parent;
+          private ImageView image;
 
 
         //create this constructor to initialize the view holder class press alt + insert to create it
@@ -50,6 +81,9 @@ public class ContactsRecViewAdapter extends RecyclerView.Adapter<ContactsRecView
             super(itemView);
 
              txtName= itemView.findViewById(R.id.txtName);
+             txtEmail = itemView.findViewById(R.id.txtEmail);
+             image = itemView.findViewById(R.id.image);
+             parent= itemView.findViewById(R.id.parent);
         }
     }
 }
--- app/src/main/java/com/example/contactrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/contactrecyclerview/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.contactrecyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     //initializing the recyclerview
@@ -15,5 +19,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         contactsRecView = findViewById(R.id.contactsRecView);
+
+        ArrayList<Contact> contacts = new ArrayList<>();
+
+        contacts.add(new Contact("rihana","riri@gmail.com","https://image.shutterstock.com/image-photo/new-york-sep-14-2017-260nw-735435583.jpg"));
+        contacts.add(new Contact("riri","riri@gmail.com","https://image.shutterstock.com/image-photo/new-york-sep-14-2017-260nw-735435583.jpg"));
+        contacts.add(new Contact("Mary","mary@gmail.com", "https://image.shutterstock.com/image-photo/new-york-sep-14-2017-260nw-735435583.jpg"));
+        contacts.add(new Contact("simi","simi@gmail.com","https://image.shutterstock.com/image-photo/new-york-sep-14-2017-260nw-735435583.jpg"));
+        ContactsRecViewAdapter adapter = new ContactsRecViewAdapter(this);
+        adapter.setContacts(contacts);
+
+        contactsRecView.setAdapter(adapter);
+        contactsRecView.setLayoutManager(new GridLayoutManager(this, 2));
     }
 }
